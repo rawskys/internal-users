090e1c3d6526632d137b2960e79bd60b2286c9a2
Pawel Rawski <pawel@rawskys.com>
2016-07-29 00:18:40 +0200

    create user

--- app/com/rawskys/controllers/InternalUsers.scala
+++ app/com/rawskys/controllers/InternalUsers.scala
@@ -1,22 +1,37 @@
 package com.rawskys.controllers
 
+import java.util.UUID
 import javax.inject.Inject
 
+import com.rawskys.model.User
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import play.modules.reactivemongo.json.collection.JSONCollection
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class InternalUsers @Inject()(val reactiveMongoApi: ReactiveMongoApi) extends Controller with MongoController with ReactiveMongoComponents {
 
+  val collection = db.collection[JSONCollection]("user")
+
   val status = Action.async {
-    val db = reactiveMongoApi.db
-    val collection = db.collection[JSONCollection]("user")
     collection.count().map { users =>
       Ok(Json.obj("users" -> users))
     }.recover {
       case e => BadRequest(Json.obj("error" -> e.getLocalizedMessage))
     }
+  }
+
+  def register = Action.async { implicit request =>
+//    implicit val messages = messagesApi.
+
+    User.form.bindFromRequest.fold(
+      errors => Future.successful(Ok(Json.obj("error" -> errors.toString))),
+      article => collection.insert(article.copy(
+        id = article.id.orElse(Some(UUID.randomUUID().toString))
+      )).map(result => Ok(Json.obj("status" -> result.message)))
+    )
   }
 }
---
+++ doc/post-to-create-user.sh
@@ -0,0 +1,1 @@
+curl -X POST -d @user.json localhost:9000/register --header "Content-Type:application/json"
